8b4659e80323d0c1c6f2988de1d8edb1d581b21b
msong793y <miles.song.ca@gmail.com>
2020-01-09 10:39:05 -0800

    update

--- classSyntax.js
+++ classSyntax.js
@@ -1,1 +1,30 @@
-console.log("hi")
+
+class Student {
+    //class constructor
+    constructor(firstName, lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.tardies = 0;
+    }
+
+    //instance method
+    fullName(){
+        return `Your full name is ${this.firstName} ${this.lastName}`;
+    }
+
+    markLate(){
+        this.tardies+=1;
+        return `${this.firstName} ${this.lastName} has been late ${this.tardies}`
+    }
+
+    //class method / utility function/
+    static enrolllStudents(...students){
+        return "Enrolling Students!"
+    }
+
+}
+
+let firstStudent = new Student("Miles", "Song")
+
+
+console.log(firstStudent)
